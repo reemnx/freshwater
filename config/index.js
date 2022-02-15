2bf9a0eedb2b654b24337b423fb950d8e746f8ab
Reem Alon <reem.alon@lilibanking.com>
2022-02-15 09:44:04 +0200

    clean config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-  public_wallet_addrress: "0x7447394099D3917c89712a16F87BCC5919F7E509",
-  wallet_mnemonic: `unfair divorce mango midnight siren gossip banana ladder account pen luggage cupboard`,
-  infura_api_key: "c1e4ced5d9214ab58c6e89807103b731",
+  public_wallet_addrress: "",
+  wallet_mnemonic: ``,
+  infura_api_key: "",
   network: "mainnet",
-  seaport_api_key: "e951a0d400934df4b0a2d338062d9add",
+  seaport_api_key: "",
 };
